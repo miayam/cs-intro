f8be6b0059f5d94c31da1edebe6f4ccac6bb717b
miayam <muhammaddeni90@gmail.com>
2023-02-06 01:40:58 +0700

    fix: Polished.

--- 002 Recursive Sort/quickSort.js
+++ 002 Recursive Sort/quickSort.js
@@ -1,13 +1,13 @@
 /**
  * Quick sort.
  * 
- * Pick a pivot item randomly in an array passed down.
- * Create an empty array called `less` to store numbers less than or equal to pivot.
- * Create an empty array called `greater` to store numbers greater than pivot.
- * Iterate through the array passed down but exclude the pivot.
- * While in the loop, compare the item to the pivot. If greater than pivot,
- * store the item into `greater`. If less than or equal to pivot, store the
- * item into `less`. Merge `less`, pivot, and `greater` recursively along
+ * Pick a `pivot` item randomly in an array passed down.
+ * Create an empty array called `less` to store numbers less than or equal to `pivot`.
+ * Create an empty array called `greater` to store numbers greater than `pivot`.
+ * Iterate through the array passed down but exclude the `pivot`.
+ * While in the loop, compare the item to the `pivot`. If greater than `pivot`,
+ * store the item into `greater`. If less than or equal to `pivot`, store the
+ * item into `less`. Merge `less`, `pivot`, and `greater` recursively along
  * the way. The process goes on and on until there's only one item left to sort.
  * @param {Number[]} array - Array of numbers
  * @returns {Number[]}
