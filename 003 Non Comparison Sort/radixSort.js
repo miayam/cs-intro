3da05ab9ec92af3813f8c25e6d64fb8dbacc57b4
miayam <muhammaddeni90@gmail.com>
2023-03-03 16:20:04 +0700

    feat: Radix sort explanation.

--- 003 Non Comparison Sort/radixSort.js
+++ 003 Non Comparison Sort/radixSort.js
@@ -1,7 +1,18 @@
+/**
+ * Get base-10 digit from a number (ones, tens, hundreds, thousands, and so on)
+ * @param {Number} number
+ * @param {Number} place - ones, tens, hundres, thousands, and so on
+ * @returns {Number}
+ */
 function getDigit(number, place) {
   return Math.floor(Math.abs(number) / Math.pow(10, place)) % 10;
 }
 
+/**
+ * Count the digits of a number
+ * @param {Number} number
+ * @returns {Number}
+ */
 function digitCount(number) {
   if (number === 0) {
     return 1;
@@ -10,25 +21,37 @@ function digitCount(number) {
   return Math.floor(Math.log10(Math.abs(number))) + 1;
 }
 
+/**
+ * Get the biggest digits from array
+ * @param {Number[]} numbers 
+ * @returns {Number} 
+ */
 function maxDigits(numbers) {
   let maxDigits = 0;
 
-  for (let i = 0; i < numbers.length; i++) {
-    maxDigits = Math.max(maxDigits, digitCount(numbers[i]));
+  for (let index = 0; index < numbers.length; index++) {
+    maxDigits = Math.max(maxDigits, digitCount(numbers[index]));
   }
 
   return maxDigits;
 }
 
+/**
+ * Radix Sort is a unique and interesting sorting algorithm,
+ * because it sorts arrays without making any comparisons
+ * between elements. 
+ * @param {Number[]} numbers - array of numbers
+ * @returns {Number}
+ */
 function radixSort(numbers) {
   const maxDigitCount = maxDigits(numbers)
 
-  for (let k = 0; k < maxDigitCount; k++) {
-    const digitBuckets = Array.from({ length: 10 }, () => []) // [[], [], [],...]
+  for (let place = 0; place < maxDigitCount; place++) {
+    const digitBuckets = Array.from({ length: 10 }, () => []); // [[], [], [],...]
 
-    for (let i = 0; i < numbers.length; i++) {
-      let digit = getDigit(numbers[i], k)
-      digitBuckets[digit].push(numbers[i])
+    for (let index = 0; index < numbers.length; index++) {
+      let digit = getDigit(numbers[index], place);
+      digitBuckets[digit].push(numbers[index]);
     }
 
     // New order after each loop
